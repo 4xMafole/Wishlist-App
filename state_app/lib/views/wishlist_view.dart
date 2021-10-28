0424ea731413ae365ce4d111774f7d466ed2908f
4xMafole <4cruxt.unknown@gmail.com>
2021-10-28 21:35:44 +0300

    Created a wishlist View.

--- state_app/lib/views/wishlist_view.dart
+++ state_app/lib/views/wishlist_view.dart
@@ -1,11 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:state_app/states/products.dart';
 
 class Wishlist extends StatelessWidget {
+  final Products _productsController = Get.find<Products>();
 
-  final Products
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Wishlist'),
+        ),
+        body: Obx(() => ListView.builder(
+            itemCount: _productsController.wishListedItems.length,
+            itemBuilder: (context, index) {
+              final item = _productsController.wishListedItems[index];
+
+              return Card(
+                key: ValueKey(item.id),
+                margin: EdgeInsets.all(5),
+                color: Colors.blue[200],
+                child: ListTile(
+                  title: Text(item.name),
+                  subtitle: Text(item.price.toStringAsFixed(2)),
+                  trailing: IconButton(
+                    icon: Icon(Icons.close),
+                    onPressed: () {
+                      item.isWishlisted.value = false;
+                      _productsController.removeItem(item.id);
+                    },
+                  ),
+                ),
+              );
+            })));
   }
 }
